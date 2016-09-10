49024f43a37a00a1b9c6b1e99d77923640da9ffe
Kevin Chung <kvnchung92@gmail.com>
2016-09-09 21:45:17 -0500

    added google places UI widget in create alarm activity, this calls a fragment and doesn't require a new page

--- app/src/main/java/mobi/smartalarm/Activities/CreateAlarm.java
+++ app/src/main/java/mobi/smartalarm/Activities/CreateAlarm.java
@@ -1,15 +1,27 @@
 package mobi.smartalarm.Activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 import mobi.smartalarm.R;
 
 public class CreateAlarm extends AppCompatActivity {
+
+    private Button destinationButton;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,23 @@ public class CreateAlarm extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //eventually create custom UI elemnts
+        context = this;
+
+        destinationButton = (Button)findViewById(R.id.destination);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Toast.makeText(context,place.toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.d("error",status.getStatusMessage());
+            }
+        });
     }
 }
